342e047cae47220722fbb571e565c5137fd55a17
sonbktt47 <163084396+sonbktt47@users.noreply.github.com>
2024-03-13 16:23:24 +0700

    Create HippoToken.sol

---
+++ HippoToken.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+contract HippoToken is ERC20, Ownable {
+    uint256 private constant HARD_CAP = 1000_000_000e18; // 1b token
+    /**
+     * @dev Constructor function of Hippo Token
+     * @dev set name, symbol, and decimal of token
+     * @dev mint total supply (cap) to address
+     */
+
+
+
+}
+
+    
+     
